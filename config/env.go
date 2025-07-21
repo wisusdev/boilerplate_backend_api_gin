5515f3a3b4089c707c1e9546be70da0212eb6d48
Jesús Avelar <wisusdev@gmail.com>
2025-07-21 12:25:19 -0600

    Elimina la función UpdateEnvFile y su lógica asociada en env.go

--- config/env.go
+++ config/env.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"strings"
 
 	"github.com/joho/godotenv"
 )
@@ -15,8 +14,6 @@ func GoDotEnv() {
 	if err != nil {
 		fmt.Println("❌ No se pudo cargar el archivo .env, usando variables de entorno del sistema")
 	}
-
-	fmt.Println("✅ Archivo .env cargado correctamente")
 }
 
 // GetEnv obtiene una variable de entorno, o retorna un valor por defecto si no existe.
@@ -64,34 +61,4 @@ func GetEnvInt(key string, defaultKey int) int {
 	}
 
 	return intVal
-}
-
-func UpdateEnvFile(key, value string) {
-	file := ".env"
-	input, err := os.ReadFile(file)
-	if err != nil {
-		fmt.Printf("No se pudo leer %s, solo se mostrará la clave.\n", file)
-		return
-	}
-	lines := strings.Split(string(input), "\n")
-	found := false
-	for i, line := range lines {
-		if strings.HasPrefix(line, key+"=") {
-			lines[i] = key + "=" + value
-			found = true
-			break
-		}
-	}
-	if !found {
-		lines = append(lines, key+"="+value)
-	}
-	output := strings.Join(lines, "\n")
-	err = os.WriteFile(file, []byte(output), 0644)
-
-	if err != nil {
-		fmt.Printf("❌ Error al actualizar %s: %v\n", file, err)
-		return
-	}
-
-	fmt.Printf("%s actualizado en %s\n", key, file)
 }
