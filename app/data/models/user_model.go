f62ded6a69185ce2b99f95b1e7552c414d98d7da
Jesús Avelar <wisusdev@gmail.com>
2025-07-29 21:18:51 -0600

    Refactor: reemplazar tipo de dato de Avatar y agregar RememberToken en UserStruct

--- app/data/models/user_model.go
+++ app/data/models/user_model.go
@@ -1,21 +1,23 @@
 package models
 
 import (
+	"semita/core/common/nulltypes"
 	"time"
 )
 
 type UserStruct struct {
-	ID              int        `json:"id" db:"INT PRIMARY KEY AUTO_INCREMENT"`
-	FirstName       string     `json:"first_name" db:"VARCHAR(255)" nullable:"false"`
-	LastName        string     `json:"last_name" db:"VARCHAR(255)" nullable:"false"`
-	Username        string     `json:"username" db:"VARCHAR(255)" unique:"true" nullable:"false"`
-	Avatar          string     `json:"avatar" db:"VARCHAR(255)" nullable:"true" default:"NULL"`
-	Language        string     `json:"language" db:"VARCHAR(10)" default:"'en'" nullable:"true"`
-	Email           string     `json:"email" db:"VARCHAR(255)" unique:"true" nullable:"false"`
-	EmailVerifiedAt *time.Time `json:"email_verified_at" db:"DATETIME" default:"NULL"`
-	Password        string     `json:"password" db:"VARCHAR(255)" nullable:"false"`
-	CreatedAt       time.Time  `json:"created_at" db:"DATETIME" default:"CURRENT_TIMESTAMP"`
-	UpdatedAt       time.Time  `json:"updated_at" db:"DATETIME" default:"CURRENT_TIMESTAMP"`
+	ID              int                  `json:"id" db:"INT PRIMARY KEY AUTO_INCREMENT"`
+	FirstName       string               `json:"first_name" db:"VARCHAR(255)" nullable:"false"`
+	LastName        string               `json:"last_name" db:"VARCHAR(255)" nullable:"false"`
+	Username        string               `json:"username" db:"VARCHAR(255)" unique:"true" nullable:"false"`
+	Avatar          nulltypes.NullString `json:"avatar" db:"VARCHAR(255)" nullable:"true" default:"NULL"`
+	Language        string               `json:"language" db:"VARCHAR(10)" default:"'en'" nullable:"true"`
+	Email           string               `json:"email" db:"VARCHAR(255)" unique:"true" nullable:"false"`
+	EmailVerifiedAt *time.Time           `json:"email_verified_at" db:"DATETIME" default:"NULL"`
+	RememberToken   nulltypes.NullString `json:"remember_token" db:"VARCHAR(100)" nullable:"true"`
+	Password        string               `json:"password" db:"VARCHAR(255)" nullable:"false"`
+	CreatedAt       time.Time            `json:"created_at" db:"DATETIME" default:"CURRENT_TIMESTAMP"`
+	UpdatedAt       time.Time            `json:"updated_at" db:"DATETIME" default:"CURRENT_TIMESTAMP"`
 }
 
 type Users []UserStruct
